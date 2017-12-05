15586c5b0c1a909a8a6392139da10c7b45a045c9
Matt Gajownik <matthew.gajownik@appster.com.au>
2017-12-06 08:50:08 +1100

    Added: Basic, barebones JS docs to config files

--- config/colors.js
+++ config/colors.js
@@ -1,5 +1,14 @@
+/** 
+	@module config/colors
+	@desc Standardised colours for bot responses.
+	Decimal format only.
+**/
+
 module.exports = {
+	/** Command completed successfully **/
 	good: 1365332,
+	/** Command completed with errors **/
 	bad: 15081504,
+	/** Command provides information **/
 	neutral: 3447003
 }
--- config/defaults.js
+++ config/defaults.js
@@ -1,6 +1,17 @@
+/** 
+	@module config/defaults
+	@desc Provide default and fallback strings
+
+	Available zone names https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+	Available format codes https://momentjs.com/docs/#/displaying/
+**/
+
 module.exports = {
+	/** Default bot timezone if server's location is unknown **/
 	zone: "Australia/Melbourne",
+	/** Default bot name format **/
 	format: "ddd h:mm A",
+	/** Based on server location, default timezone to speed up initial setup **/
 	zones: {
 		'brazil': 'America/Araguaina',
 		'eu-central': 'Europe/Berlin',
